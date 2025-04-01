b571f9c3aaad881ca8e9563cfdc431265507db80
Yevhen <evgenribacuk@gmail.com>
2025-04-01 22:09:17 +0300

    final 5 laba

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <string>
 
 using namespace std;
@@ -15,10 +14,10 @@ protected:
     string name;
 public:
     Person(string n = "Unknown") : name(n) {
-        cout << "Person created: " << name << endl;
+        //cout << "Person created: " << name << endl;
     }
     virtual ~Person() {
-        cout << "Person destroyed: " << name << endl;
+       // cout << "Person destroyed: " << name << endl;
     }
 
     virtual void display() const {
@@ -27,30 +26,26 @@ public:
     virtual void work() const {
         cout << name << " is doing general tasks." << endl;
     }
-
-    virtual string getRole() const = 0;
+    void staticBindingExample() {
+        cout << "I am a Person" << endl;
+    }
 };
 
 class Employee : public Person, public Workable {
 protected:
     string position;
     double salary;
-    static int employeeCount;
 public:
     Employee(string n = "Unknown", string p = "Unknown", double s = 0.0)
         : Person(n), position(p), salary(s) {
-        employeeCount++;
-        cout << "Employee created: " << name << endl;
+       // cout << "Employee created: " << name << endl;
     }
     Employee(const Employee &e) : Person(e.name), position(e.position), salary(e.salary) {
-        employeeCount++;
         cout << "Employee copied: " << name << endl;
     }
     virtual ~Employee() {
-        cout << "Employee destroyed: " << name << endl;
-        employeeCount--;
+      //  cout << "Employee destroyed: " << name << endl;
     }
-    static int getEmployeeCount() { return employeeCount; }
 
     virtual void display() const override {
         cout << "Employee - Name: " << name << ", Position: " << position << ", Salary: " << salary << endl;
@@ -59,22 +54,22 @@ public:
         cout << name << " is working as " << position << "." << endl;
     }
     virtual void doWork() const override {
-        cout << name << " is actively performing work tasks!" << endl;
+        cout << name << "" << endl;
     }
-    virtual string getRole() const override {
-        return "Employee";
+    void staticBindingExample() {
+        cout << "I am an Employee" << endl;
     }
+
     string getName() const { return name; }
     double getSalary() const { return salary; }
 };
-int Employee::employeeCount = 0;
 
 class Manager : public Employee {
 private:
     int teamSize;
 public:
     Manager(string n, string p, double s, int t) : Employee(n, p, s), teamSize(t) {
-        cout << "Manager created: " << name << endl;
+      //  cout << "Manager created: " << name << endl;
     }
     virtual void display() const override {
         cout << "Manager - Name: " << name << ", Position: " << position
@@ -83,11 +78,8 @@ public:
     virtual void work() const override {
         cout << name << " is managing a team of " << teamSize << " people." << endl;
     }
-    virtual string getRole() const override {
-        return "Manager";
-    }
     virtual ~Manager() {
-        cout << "Manager destroyed: " << name << endl;
+       // cout << "Manager destroyed: " << name << endl;
     }
 };
 
@@ -96,31 +88,46 @@ private:
     string department;
 public:
     Intern(string n, string dept) : Person(n), department(dept) {
-        cout << "Intern created: " << name << " in department " << department << endl;
+      //  cout << "Intern created: " << name << " in department " << department << endl;
     }
     virtual void display() const override {
         cout << "Intern - Name: " << name << ", Department: " << department << endl;
     }
     virtual void work() const override final {
-        cout << "Intern " << name << " is learning and doing small tasks." << endl;
+        cout << "Intern " << name << " doing small tasks." << endl;
     }
     virtual void doWork() const override {
-        cout << "Intern " << name << " is assisting in the " << department << " department!" << endl;
-    }
-    virtual string getRole() const override {
-        return "Intern";
+        cout << "Intern " << name << " is assisting in the " << department << " department" << endl;
     }
     virtual ~Intern() {
-        cout << "Intern destroyed: " << name << endl;
+      //  cout << "Intern destroyed: " << name << endl;
     }
 };
 
-// class SeniorIntern : public Intern {
-//public:
-//  SeniorIntern(string n, string p, double s) : Intern(n, p, s) {
-//    cout << "SeniorIntern created: " << name << endl;
-//}
-//};
+/*class Student : public Intern {
+private:
+    string faculty;
+public:
+    Student(string n, string dept, string fac) : Intern(n, dept), faculty(fac) {
+        // cout << "Student created: " << name << " in department " << department << ", Faculty: " << faculty << endl;
+    }
+    virtual void display() const override {
+        cout << "Student - Name: " << name << ", Department: " << department << ", Faculty: " << faculty << endl;
+    }
+    virtual void work() const override final {
+        cout << "Student " << name << " is studying and contributing in the " << department << " department." << endl;
+    }
+    virtual void doWork() const override {
+        cout << "Student " << name << " is researching for the " << faculty << " faculty and assisting the " << department << " department." << endl;
+    }
+    virtual string getRole() const override {
+        return "Student";
+    }
+    virtual ~Student() {
+        // cout << "Student destroyed: " << name << endl;
+    }
+};*/
+
 
 class Contractor : public Person, public Workable {
 private:
@@ -130,52 +137,51 @@ private:
 public:
     Contractor(string n, string spec, double rate, int duration)
         : Person(n), specialization(spec), hourlyRate(rate), contractDuration(duration) {
-        cout << "Contractor created: " << name << " (Specialization: " << specialization << ")" << endl;
+      //  cout << "Contractor created: " << name << " (Specialization: " << specialization << ")" << endl;
     }
     virtual void display() const override {
         cout << "Contractor - Name: " << name << ", Specialization: " << specialization
              << ", Hourly Rate: $" << hourlyRate << ", Contract Duration: " << contractDuration << " months" << endl;
     }
     virtual void work() const override {
-        cout << "Contractor " << name << " is working on a temporary project." << endl;
+        cout << "Contractor " << name << " is working on a project." << endl;
     }
     virtual void doWork() const override {
-        cout << "Contractor " << name << " is completing freelance tasks for the company!" << endl;
-    }
-    virtual string getRole() const override {
-        return "Contractor";
+        cout << "Contractor " << name << " is completing tasks for the company" << endl;
     }
     virtual ~Contractor() {
-        cout << "Contractor destroyed: " << name << endl;
+      //  cout << "Contractor destroyed: " << name << endl;
     }
 };
 
 void performWork(const Workable& worker) {
     worker.doWork();
-}
+   }
 
 int main() {
     Employee emp1("Vasya Pupkin", "Software Engineer", 75000);
     emp1.display();
-    cout << "Total Employees: " << Employee::getEmployeeCount() << endl;
 
     Manager mgr1("Walter White", "Project Manager", 90000, 10);
     mgr1.display();
 
-    Intern intern1("Johnny", "IT");
-    performWork(intern1);
+    Intern intern1("Dima", "IT");
     intern1.display();
 
-    Contractor contractor1("Alice", "Web Development", 50.0, 6);
-    performWork(contractor1);
+    Contractor contractor1("Alina", "Web Development", 50.0, 6);
     contractor1.display();
 
-    cout << emp1.getName() << " is an " << emp1.getRole() << endl;
-    cout << mgr1.getName() << " is a " << mgr1.getRole() << endl;
-    cout << intern1.getRole() << " is an Intern" << endl;
-    cout << contractor1.getRole() << " is a Contractor" << endl;
+    performWork(intern1);
+    performWork(contractor1);
+
+
+    Person* basePtr = new Manager("Sasha", "Lead", 100000, 5);
+    basePtr->work();
+    delete basePtr;
+
+    Person* p = new Employee("Johnny Sins", "Developer", 50000);
+    p->staticBindingExample();
+    delete p;
 
     return 0;
 }
-
-
